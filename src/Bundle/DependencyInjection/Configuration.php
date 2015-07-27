ca01380ef68fc40b15281ec2f682d85a248914fa
Sullivan SENECHAL <sullivan@nexylan.com>
2015-07-27 09:57:12 +0200

    Bad configuration fix

--- src/Bundle/DependencyInjection/Configuration.php
+++ src/Bundle/DependencyInjection/Configuration.php
@@ -25,11 +25,11 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('nexy_cloudflare');
+        $rootNode = $treeBuilder->root('nexy_cloudflare_host_gw');
 
         $rootNode
             ->children()
-                ->scalarNode('host_key')->cannotBeEmpty()->end()
+                ->scalarNode('host_key')->isRequired()->cannotBeEmpty()->end()
             ->end()
         ;
 
